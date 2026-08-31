8359ad3216b424b7e49791713b21117878fb583b
Kinza Afzal <148489795+kinza7124@users.noreply.github.com>
2026-08-31 14:45:44 +0500

    Add files via upload

---
+++ OOP/Lecture/WEEK-02/temp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class temp{
+	public:
+		int a=0;
+		float f=0;
+	public:
+		void seta(int i){
+			a=0;
+		}
+		void setf(float i){
+			f=0;
+		}
+		void get(){
+			cout<<"a="<<a<<" f="<<f;
+		}
+};
+
